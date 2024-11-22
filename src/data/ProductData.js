1cdb4f4dbff255730ed030eb2cfc09c2922c0fed
Rushil patel <patelrushil1510@gmail.com>
2024-11-22 11:08:02 +0530

    first commit

---
+++ src/data/ProductData.js
@@ -0,0 +1,47 @@
+export const products = [
+    {
+        id: 1,
+        title: 'Classic Black Shirt Jacket',
+        price: 299,
+        image: 'images/G-1.jpg',
+
+    },
+    {
+        id: 2,
+        title: 'Vision Patterned Long Sleeve',
+        price: 99,
+        image: 'images/g-2.jpg',
+
+    },
+    {
+        id: 3,
+        title: 'Classic Hooded Regular Long Sleeve',
+        price: 239,
+        image: 'images/g-3.jpg',
+
+    }, {
+        id: 4,
+        title: 'Aesthetic Seam Sneakers',
+        price: 139,
+        image: 'images/g-4.jpg',
+
+    }, {
+        id: 5,
+        title: 'Classic Shoulder bag',
+        price: 239,
+        image: 'images/g-5.jpg',
+
+    },{
+        id: 6,
+        title: 'Cool sunglasses',
+        price: 79,
+        image: 'images/g-6.jpg',
+
+    },{
+        id: 7,
+        title: 'Raffia Earrings',
+        price: 79,
+        image: 'images/g-8.jpg',
+
+    }
+]
